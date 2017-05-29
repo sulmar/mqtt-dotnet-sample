1cd6920ead6ff64e57eb8db4b5cb5481a00f8d81
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-05-29 23:13:32 +0200

    Add project files.

---
+++ mqtt-dotnet-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mqtt_dotnet_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
